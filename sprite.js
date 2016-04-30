d0d6504282ddbbb7987a0161138d1bc93da20d46
Noah Kim <noahbkim@gmail.com>
2016-04-30 10:12:38 -0400

    Worked on sprites and animation.

--- sprite.js
+++ sprite.js
@@ -2,11 +2,11 @@
 function Sprite(x, y, w, h, rx, ry) {
 
     /* Position (center). */
-    this.position = new Vector(x || 0, y || 0);
+    this.pos = new Vector(x || 0, y || 0);
     
     /* Rotation about the center. */
     this.rotation = 0;
-    this.rposition = new Vector(rx || w/2 || 0, ry || h/2 || 0);
+    this.rpos = new Vector(rx || w/2 || 0, ry || h/2 || 0);
     
     /* Size. */
     this.width = w || 0;
@@ -39,24 +39,24 @@ function Sprite(x, y, w, h, rx, ry) {
     
     /* Moves sprite by the specified amount in each direction. */
     this.translate = function(dx, dy) {
-        this.position.x += dx;
-        this.position.y += dy;
+        this.pos.x += dx;
+        this.pos.y += dy;
     }
     
     /* Bounding box. */
     this.bbox = function() {
-        var leftX = this.position.x - this.width/2;
-        var topY = this.position.y - this.height/2;
+        var leftX = this.pos.x - this.width/2;
+        var topY = this.pos.y - this.height/2;
         var box = [leftX * Math.cos(this.rotation) - topY * Math.sin(this.rotation), 
                     leftX * Math.sin(this.rotation) + topY * Math.cos(this.rotation), this.width, this.height];
     }
     
     /* Returns whether this sprite touches another one. */
     this.touches = function(sprite) {
-        return !(this.position.x + this.width < sprite.position.x || 
-                 this.position.x > sprite.position.x + sprite.width || 
-                 this.position.y + this.height < sprite.position.y || 
-                 this.position.y > sprite.position.y + sprite.height);
+        return !(this.pos.x + this.width < sprite.pos.x || 
+                 this.pos.x > sprite.pos.x + sprite.width || 
+                 this.pos.y + this.height < sprite.pos.y || 
+                 this.pos.y > sprite.pos.y + sprite.height);
     }
     
       
@@ -71,23 +71,28 @@ function Sprite(x, y, w, h, rx, ry) {
     this.render = function(context) {
 		if (this.spriteImage != null) {
             
-            context.translate(this.position.x+this.rposition.x, this.postition.y+this.position.y);
-            context.rotate(this.rotation);
-            context.translate(-this.position.x-this.rposition.x, -this.position.y-this.rposition.y);
+            context.save();
+            context.translate(this.pos.x, this.pos.y);
+            context.rotate(-this.rotation);
+            context.translate(-this.pos.x, -this.pos.y);
             
 			if (this.currentAnimation in this.animations) { // There's an animation
-				var f = this.animations[this.currentAnimation].getCurrentFrame();
+			
+                var f = this.animations[this.currentAnimation].getCurrentFrame();
 				
 				// Get the subimage information
 				var subWidth = this.spriteImage.width / this.numColumns;
 				var subHeight = this.spriteImage.height / this.numRows;
 				var subX = (f % this.numColumns) * subWidth;
-				var subY = ((f / this.numRows) | 0) * subHeight;
+				var subY = ((f / this.numRows) || 0) * subHeight;
 				
 				// Draw the subimage
-				context.drawImage(this.spriteImage, subX, subY, subWidth, subHeight, this.position.x, this.position.y, this.width, this.height);
+				context.drawImage(this.spriteImage, subX, subY, subWidth, subHeight, 
+                                  this.pos.x-this.rpos.x, this.pos.y-this.rpos.y, this.width, this.height);
+                
 			} else { // Image but no animation
-				context.drawImage(this.spriteImage, 0, 0, this.spriteImage.width, this.spriteImage.height, this.position.x, this.position.y, this.width, this.height);
+				context.drawImage(this.spriteImage, 0, 0, this.spriteImage.width, this.spriteImage.height, 
+                                  this.pos.x-this.rpos.x, this.pos.y-this.rpos.y, this.width, this.height);
 			}
             
             context.restore();
