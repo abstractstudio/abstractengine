230830cc173d1a2e5e2b525e823015b913b7d3d9
Noah Kim <noahbkim@gmail.com>
2016-05-01 14:13:42 -0400

    Fixed spacing.

--- animation.js
+++ animation.js
@@ -7,48 +7,48 @@ function Animation(name, frames) {
     this.loop = true;
     
     /* Frame specific. */
-	this.index = 0;
-	this.indices = frames || [];
-	this.speed = 0.02;
-	this.start = 0;
-	
-	/** Starts this animation from the beginning. */
-	this.play = function() {
-		this.index = 0;
-		this.start = Date.now();
-		this.playing = true;
-	}
-	
-	/** Gets the index of the current animation frame in the spritesheet. */
-	this.frame = function() {
-	    return this.indices[this.index];
-	}
-	
-	/** Moves the animation forward. */
-	this.update = function() {
+    this.index = 0;
+    this.indices = frames || [];
+    this.speed = 0.02;
+    this.start = 0;
+    
+    /** Starts this animation from the beginning. */
+    this.play = function() {
+        this.index = 0;
+        this.start = Date.now();
+        this.playing = true;
+    }
+    
+    /** Gets the index of the current animation frame in the spritesheet. */
+    this.frame = function() {
+        return this.indices[this.index];
+    }
+    
+    /** Moves the animation forward. */
+    this.update = function() {
         
         /* Only change if playing. */
-	    if (this.playing) {
+        if (this.playing) {
             
             /* Check if time to update. */
-    	    var now = Date.now();
-    	    if (now - this.start >= this.speed * 1000) {
+            var now = Date.now();
+            if (now - this.start >= this.speed * 1000) {
                 
                 /* Set start time and increment index. */
-    	        this.start = now;
-    	        this.index++;
+                this.start = now;
+                this.index++;
                 
                 /* Loop if should. */
-    	        if (this.index >= this.indices.length) {
-    	            if (this.loop) this.index = 0;
-    	            else this.playing = false;
-    	        }
+                if (this.index >= this.indices.length) {
+                    if (this.loop) this.index = 0;
+                    else this.playing = false;
+                }
                 
-    	    }
+            }
             
-	    }
+        }
         
-	}
-	
-	
+    }
+    
+    
 }
