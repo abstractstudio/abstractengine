511bc0fb4cf9f04905f17302c4940fea3b19c97b
Noah Kim <noahbkim@gmail.com>
2016-05-20 15:19:15 -0400

    Might as well.

---
+++ sprite.js
@@ -0,0 +1,11 @@
+/* Custom entity tools. */
+var sprite = {};
+
+/* The basic sprite. */
+sprite.Sprite = function Sprite() {
+
+	/* Basic transform. */
+	this.pos = new geometry.Vector();
+	this.rot = 0.0;
+
+}
