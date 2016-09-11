c38c88b5917a0bbd70dd96330dc1753b89b90a04
Noah Kim <noahbkim@gmail.com>
2016-09-11 12:54:58 -0400

    Fleshed out the demo.

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,12 @@ goog.require("engine.Engine");
 
 class Demo extends Engine {
     
+    setup() {}
+    
+    load() {}
+    
+    update(delta) {}
+    
     render(context) {    
         context.fillStyle = "white";
         context.fillRect(0, 0, this.canvas.width, this.canvas.height);
@@ -10,6 +16,10 @@ class Demo extends Engine {
         context.textBaseline = "hanging";
         context.fillText(Math.round(this.fps()) + "", 10, 10);
     }
+    
+    stop() {}
+    
+    cleanup() {}
     
 }
 
