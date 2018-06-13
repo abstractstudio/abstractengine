8c1784f96e7a18ef26cc175db151181e1b3bad73
jasmaa <jasonmaa3955@gmail.com>
2018-06-13 12:36:07 -0400

    Add simple drawing demo

---
+++ demos/simple/src/demo.js
@@ -0,0 +1,44 @@
+/** simple.js */
+// Simple drawing demonstration with Transform2D
+
+goog.require("engine.Engine2D");
+goog.require("engine.Vector2D");
+
+class Simple extends Engine2D {
+
+		// Set up position and direction vecs
+		setup() {
+			position = new Vector2D(0, 0);
+			direction = new Vector2D(3, 3);
+		}
+
+		update(delta) {
+			// Move square
+			position.add(direction);
+
+			// Bounce on edges
+			if (position.x > this.canvas.width || position.x < 0){
+				direction.x = -direction.x;
+			}
+			if (position.y > this.canvas.height || position.y < 0){
+				direction.y = -direction.y;
+			}
+		}
+
+    render(context, canvas) {
+				// Render BG
+        context.fillStyle = "white";
+        context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        context.fillStyle = "black";
+        context.font = "20px Verdana";
+        context.textBaseline = "hanging";
+        context.fillText("Simple", 10, 10);
+
+				// Render square
+				context.fillStyle = "red";
+				context.fillRect(position.x, position.y, 10, 10);
+    }
+
+}
+
+window.Simple = Simple;
--- demos/test/src/test.js
+++ demos/test/src/test.js
@@ -1,16 +1,19 @@
+/** test.js */
+// Displays frames per second in empty canvas
+
 goog.require("engine.Engine2D");
 
 class Demo extends Engine2D {
-        
-    render(context, canvas) {    
+
+    render(context, canvas) {
         context.fillStyle = "white";
         context.fillRect(0, 0, this.canvas.width, this.canvas.height);
         context.fillStyle = "black";
         context.font = "20px Verdana";
         context.textBaseline = "hanging";
-        context.fillText(Math.round(this.fps()) + "", 10, 10);
+        context.fillText("FPS: " + Math.round(this.fps()), 10, 10);
     }
-    
+
 }
 
 window.Demo = Demo;
